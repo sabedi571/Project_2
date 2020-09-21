e7b25c935fb3e13015e17f80651cc6f96ec8ae36
Red Fairy <agorelik2@gmail.com>
2020-09-21 19:37:34 -0400

    replaced files

--- models/event.js
+++ models/event.js
@@ -1,1 +1,22 @@
-// the model for event
+// module.exports = function (sequelize, DataTypes) {
+//   var Event = sequelize.define("Event", {
+//     name: DataTypes.STRING,
+//     movie: DataTypes.STRING,
+//     dateAndTime: DataTypes.DATE,
+//     location: DataTypes.STRING,
+//     description: DataTypes.TEXT,
+//     user: DataTypes.STRING,
+//     fullname: DataTypes.STRING,
+//   });
+
+//   Event.associate = function (models) {
+//     Event.hasMany(models.Comment, {
+//       onDelete: "cascade",
+//     }),
+//       Event.hasMany(models.Invitee, {
+//         onDelete: "cascade",
+//       });
+//   };
+
+//   return Event;
+// };

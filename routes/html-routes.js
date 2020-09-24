eaacc01f75b92b789fbe11c5e8a3136551d19c25
Red Fairy <agorelik2@gmail.com>
2020-09-24 14:29:19 -0400

    added code to have all the users on a dropdown box

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -6,7 +6,6 @@ var db = require("../models");
 // Requiring our custom middleware for checking if a user is logged in
 var isAuthenticated = require("../config/middleware/isAuthenticated");
 
-
 module.exports = function (app) {
   app.get("/", function (req, res) {
     // If the user already has an account send them to the members page
@@ -17,7 +16,6 @@ module.exports = function (app) {
     res.render("signup");
     //res.sendFile(path.join(__dirname, "../public/signup.html"));
   });
-
 
   app.get("/login", function (req, res) {
     // If the user already has an account send them to the members page
@@ -46,19 +44,42 @@ module.exports = function (app) {
   //app.get("/newevent", isAuthenticated, function (req, res) {
   //res.sendFile(path.join(__dirname, "../public/members.html"));
 
+  //Get Event by ID passed in url
+  // app.get("/event/:id", function (req, res) {
+  //   db.Event.findOne({
+  //     where: { id: req.params.id },
+  //     //include: [db.Comment, db.Invitee],
+  //   }).then(function (dbEvent) {
+  //     //console.log (req.body)
+  //     console.log(dbEvent.name);
+  //   });
+  // });
+
   // load newEvent page
-  app.get("/newevent", function (req, res) {
+  app.get("/newevent", function (req, res, next) {
     db.User.findAll({}).then(function (dbUser) {
-      res.render("newevent", {
-        users: dbUser,
+      let users = [];
+      console.log("retrieved users: " + dbUser);
+
+      dbUser.forEach(function (user) {
+        users.push({
+          id: user.id,
+          firstname: user.firstname,
+          lastname: user.lastname,
+        });
       });
+
+      res.render("newevent", {
+        users: users, // updated AG
+        //users: dbEvent.User,
+      });
+      console.log("////////////");
+      console.log(users);
+      console.log("////////////");
     });
   });
   // Render 404 page for any unmatched routes
   app.get("*", function (req, res) {
     res.render("404");
-
-
   });
-
 };

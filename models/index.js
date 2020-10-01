cfa192dd11e44c1bdda91eddc2200e04fa7e4096
Red Fairy <agorelik2@gmail.com>
2020-10-01 18:45:12 -0400

    modified models/index.js to use environmental var

--- models/index.js
+++ models/index.js
@@ -8,13 +8,23 @@ var env = process.env.NODE_ENV || "development";
 var config = require(__dirname + "/../config/config.js")[env];
 var db = {};
 
-  const sequelize = new Sequelize(
+// const sequelize = new Sequelize(
+//   config.database,
+//   config.username,
+//   config.password,
+//   config
+// );
+
+if (config.use_env_variable) {
+  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+} else {
+  var sequelize = new Sequelize(
     config.database,
     config.username,
     config.password,
     config
   );
-
+}
 
 fs.readdirSync(__dirname)
   .filter(function (file) {
